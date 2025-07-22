f04afa0fa4ca5c711b6c6b019b9d398246bc9cd5
namnntt <namnntt@gmail.com>
2025-07-22 09:21:41 +0700

    sua lai ham main

--- CSNewsWatcher/Program.cs
+++ CSNewsWatcher/Program.cs
@@ -4,24 +4,23 @@ namespace CSNewsWatcher
 {
     internal class Program
     {
+
         static async Task Main(string[] args)
         {
-            static async Task Main(string[] args)
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            await new BrowserFetcher().DownloadAsync();
+            Console.WriteLine("Trình duyệt đã sẵn sàng.");
+
+            var watcher = new CSWatcherVer1();
+
+            while (true)
             {
-                Console.OutputEncoding = System.Text.Encoding.UTF8;
-
-                await new BrowserFetcher().DownloadAsync();
-                Console.WriteLine("Trình duyệt đã sẵn sàng.");
-
-                var watcher = new CSWatcherVer1();
-
-                while (true)
-                {
-                    await watcher.CheckAndNotify();
-                    Console.WriteLine("------------------------------------------");
-                    await Task.Delay(TimeSpan.FromMinutes(1));
-                }
+                await watcher.CheckAndNotify();
+                Console.WriteLine("------------------------------------------");
+                await Task.Delay(TimeSpan.FromMinutes(1));
             }
         }
+
     }
 }
